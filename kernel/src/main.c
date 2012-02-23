fe389c0b087c6ca8012363a258282e9a6db8ea87
Hailey Somerville <hailey@hailey.lol>
2012-02-23 17:54:28 +1100

    hello world kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,6 +1,20 @@
 #include "multiboot.h"
+#include <stdio.h>
+#include <string.h>
 
-void kmain(struct multiboot_info* mbd, unsigned int magic)
+void write_string(char* s)
 {
-    for(;;);
+    uint8_t* vram = (uint8_t*)0xb8000;
+    size_t i, len = strlen(s);
+    for(i = 0; i < len; i++) {
+        vram[i * 2] = s[i];
+        vram[i * 2 + 1] = 15;
+    }
+}
+
+void kmain(struct multiboot_info* mbd, uint32_t magic)
+{
+    char buff[100];
+    snprintf(buff, 100, "Hello %s!", "world");
+    write_string(buff);
 }
