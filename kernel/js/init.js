f3e154dbd11ff1775ef569ca0514bdb2791c8115
charliesome <charlie@charliesomerville.com>
2012-02-25 01:53:08 +1100

    PIT tick

--- kernel/js/init.js
+++ kernel/js/init.js
@@ -1,6 +1,7 @@
 console.log("Hello world from JavaScript!");
 console.log("My bytecode is " + Kernel.modules["/kernel/init.jmg"].length + " bytes long");
 
+var tick = 0;
 Kernel.isrs[32] = function() {
-    console.log("hello!");
+    console.log("tick " + tick++);
 };
