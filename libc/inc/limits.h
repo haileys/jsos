d1f917deeaf10879a637328d18b3770a9fe05362
charliesome <charlie@charliesomerville.com>
2012-02-24 21:15:11 +1100

    libc additions

--- libc/inc/limits.h
+++ libc/inc/limits.h
@@ -4,4 +4,20 @@
 // this libc is only intended to run on 32 bit
 #define __WORDSIZE 32
 
+#define CHAR_BIT    8
+#define SCHAR_MIN   (-128)
+#define SCHAR_MAX   127
+#define UCHAR_MAX   255
+#define CHAR_MIN    SCHAR_MIN
+#define CHAR_MAX    SCHAR_MAX
+#define SHRT_MIN    (-32768)
+#define SHRT_MAX    32767
+#define USHRT_MAX   65535
+#define INT_MIN     (-2147483648)
+#define INT_MAX     2147483647
+#define UINT_MAX    4294967295U
+#define LONG_MIN    INT_MIN
+#define LONG_MAX    INT_MAX
+#define ULONG_MAX   UINT_MAX
+
 #endif
--- libc/inc/string.h
+++ libc/inc/string.h
@@ -9,6 +9,7 @@ int strcmp(const char* a, const char* b);
 
 void* memset(void* ptr, char c, size_t bytes);
 void* memcpy(void* dest, const void* source, size_t bytes);
+void* memmove(void* dest, const void* source, size_t bytes);
 int memcmp(const void* a, const void* b, size_t bytes);
 
 #endif
