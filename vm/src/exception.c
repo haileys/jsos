176fbe7b168ce9eafc32a2bff9104654ff0db081
charliesome <charlie@charliesomerville.com>
2012-02-24 21:14:56 +1100

    js_set_panic_handler

--- vm/src/exception.c
+++ vm/src/exception.c
@@ -21,13 +21,23 @@ void js_set_exception_handler(js_exception_handler_t* exception_handler)
     handler = exception_handler;
 }
 
+static void(*js_panic_handler)(const char*, char*, int, char*);
+
+void js_set_panic_handler(void(*panic_handler)(const char*, char*, int, char*))
+{
+    js_panic_handler = panic_handler;
+}
+
 void js_panic_impl(const char* func, char* file, int line, char* fmt, ...)
 {
-    char buff[1024];
+    char buff[2048];
     va_list va;
     va_start(va, fmt);
-    vsnprintf(buff, 1023, fmt, va);
+    vsnprintf(buff, 2047, fmt, va);
     va_end(va);
+    if(js_panic_handler) {
+        js_panic_handler(func, file, line, buff);
+    }
     printf("[PANIC] %s\n        in %s() at %s:%d\n", buff, func, file, line);
     exit(-1);
 }
