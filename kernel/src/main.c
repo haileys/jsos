0e37c655d0748e6c8bfe7fab9eb48c676e94b4e7
charliesome <charlie@charliesomerville.com>
2012-02-24 23:15:45 +1100

    loads js

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,6 +1,7 @@
 #include <gc.h>
 #include <vm.h>
 #include <value.h>
+#include <exception.h>
 #include <stdarg.h>
 #include "multiboot.h"
 #include "mm.h"
@@ -15,6 +16,19 @@ static VAL console_log(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* a
     }
     kprintf("\n");
     return js_value_undefined();
+}
+
+void load_modules(VAL object, multiboot_module_t* modules, uint32_t count)
+{
+    kprintf("Loading %d modules:\n", count);
+    uint32_t i;
+    for(i = 0; i < count; i++) {
+        kprintf("  %s\n", modules[i].cmdline);
+        js_string_t* name = js_cstring((char*)modules[i].cmdline);
+        uint32_t size = modules[i].mod_end - modules[i].mod_start;
+        VAL data = js_value_make_string((char*)modules[i].mod_start, size);
+        js_object_put(object, name, data);
+    }
 }
 
 void kmain_(struct multiboot_info* mbd, uint32_t magic)
@@ -32,9 +46,34 @@ void kmain_(struct multiboot_info* mbd, uint32_t magic)
     js_gc_init(&dummy);
     js_vm_t* vm = js_vm_new();
     
-    VAL console = js_value_make_object(js_value_null(), js_value_null());
+    VAL console = js_make_object(vm);
     js_object_put(console, js_cstring("log"), js_value_make_native_function(vm, NULL, js_cstring("log"), console_log, NULL));
     js_object_put(vm->global_scope->global_object, js_cstring("console"), console);
+    
+    VAL Kernel = js_make_object(vm);
+    js_object_put(vm->global_scope->global_object, js_cstring("Kernel"), Kernel);
+    
+    VAL modules = js_make_object(vm);
+    js_object_put(Kernel, js_cstring("modules"), modules);
+    load_modules(modules, (multiboot_module_t*)mbd->mods_addr, mbd->mods_count);
+    
+    VAL vinit = js_object_get(modules, js_cstring("/kernel/init.jmg"));
+    if(js_value_get_type(vinit) != JS_T_STRING) {
+        panic("could not load /kernel/init.jmg");
+    }
+    js_value_t* sinit = js_value_get_pointer(vinit);
+    js_image_t* image = js_image_parse(sinit->string.buff, sinit->string.length);
+    
+    kprintf("Loaded /kernel/init.jmg\n");
+    
+    VAL exception, retn;
+    JS_TRY({
+        retn = js_vm_exec(vm, image, 0, vm->global_scope, js_value_null(), 0, NULL);
+    }, exception, {
+        panicf("Unhandled exception: %s", js_value_get_pointer(js_to_string(exception))->string.buff);
+    });
+    
+    kprintf("Executed init.jmg, returned: %s\n", js_value_get_pointer(js_to_string(retn))->string.buff);
 }
 
 void kmain(struct multiboot_info* mbd, uint32_t magic)
