10c535dbbf2ced73caa4895afebfc841e3093612
charliesome <charlie@charliesomerville.com>
2012-02-25 01:43:26 +1100

    PIT from javascript

--- kernel/js/init.js
+++ kernel/js/init.js
@@ -1,3 +1,6 @@
 console.log("Hello world from JavaScript!");
 console.log("My bytecode is " + Kernel.modules["/kernel/init.jmg"].length + " bytes long");
 
+Kernel.isrs[32] = function() {
+    console.log("hello!");
+};
