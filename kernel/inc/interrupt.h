f937c4b753d1c64c9aa25f6494f1aab5cfdef129
Hailey Somerville <hailey@hailey.lol>
2012-02-25 01:42:56 +1100

    move calls to idt_init, bind idt_init to js

--- kernel/inc/interrupt.h
+++ kernel/inc/interrupt.h
@@ -26,7 +26,7 @@ typedef struct idt_entry {
     uint16_t offset_hi;
 } __attribute__((__packed__)) idt_entry_t;
 
-void idt_init();
+void idt_init(js_vm_t* vm);
 void idt_set_gate(uint8_t gate, idt_entry_t entry);
 void idt_register_handler(uint8_t gate, uint32_t isr);
 uint32_t isr_dispatch(uint32_t interrupt, uint32_t error);
