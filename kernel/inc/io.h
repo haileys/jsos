33bfa9a0c84666f7383da92e990420dbf417d4ae
charliesome <charlie@charliesomerville.com>
2012-02-24 21:47:17 +1100

    add volatile to outb

--- kernel/inc/io.h
+++ kernel/inc/io.h
@@ -5,7 +5,7 @@
 
 static inline void outb(uint16_t port, uint8_t value)
 {
-	__asm__("outb %0, %1" : : "a"(value), "Nd"(port) );
+	__asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port) );
 }
 
 static inline uint8_t inb(uint16_t port)
