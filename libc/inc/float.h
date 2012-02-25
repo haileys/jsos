521562abbb1206db91ec487f8a2c229dd3e17a70
charliesome <charlie@charliesomerville.com>
2012-02-25 22:07:39 +1100

    flaot.h

---
+++ libc/inc/float.h
@@ -0,0 +1,26 @@
+#ifndef LIBC_FLOAT_H
+#define LIBC_FLOAT_H
+
+#define IEEE_8087
+
+#define FLT_RADIX       2
+#define FLT_MANT_DIG    24
+#define DBL_MANT_DIG    53
+#define FLT_DIG         6
+#define DBL_DIG         15
+#define FLT_EPSILON     1.19209290e-07F
+#define DBL_EPSILON     2.2204460492503131e-16
+#define FLT_MIN         1.17549435e-38F
+#define DBL_MIN         2.2250738585072014e-308
+#define FLT_MAX         3.40282347e+38F
+#define DBL_MAX         1.7976931348623157e+308
+#define FLT_MIN_EXP     (-125)
+#define DBL_MIN_EXP     (-1021)
+#define FLT_MIN_10_EXP  (-37)
+#define DBL_MIN_10_EXP  (-307)
+#define FLT_MAX_EXP     128
+#define DBL_MAX_EXP     1024
+#define FLT_MAX_10_EXP  38
+#define DBL_MAX_10_EXP  308
+
+#endif
