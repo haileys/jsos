49333a4ffee290012a49e7518ece285d4a3e85cd
Hailey Somerville <hailey@hailey.lol>
2012-02-25 22:49:08 +1100

    use a ring buffer for dispatching interrupt events

--- kernel/src/interrupt.c
+++ kernel/src/interrupt.c
@@ -10,6 +10,16 @@ static idt_entry_t idt[256];
 
 static VAL js_isr_table;
 
+#define MAX_QUEUED_INTERRUPTS 16
+
+struct {
+    uint32_t interrupt;
+    uint32_t error;
+} queued_interrupts[MAX_QUEUED_INTERRUPTS];
+
+uint32_t queue_front;
+uint32_t queue_back;
+
 void idt_set_gate(uint8_t gate, idt_entry_t entry)
 {
     idt[gate] = entry;
@@ -57,14 +67,43 @@ void idt_register_handler(uint8_t gate, uint32_t isr)
 uint32_t isr_dispatch(uint32_t interrupt, uint32_t error)
 {
     if(interrupt == 39) {
-        return interrupt;
+        return 0;
     }
-    char buff[8];
-    itoa(interrupt, buff, 10);
-    VAL isr = js_object_get(js_isr_table, js_cstring(buff));
-    VAL errcode = js_value_make_double(error);
-    if(js_value_get_type(isr) == JS_T_FUNCTION) {
-        js_call(isr, js_isr_table, 1, &errcode);
+    
+    // isr_dispatch is already in a cli/hlt so there's no need to do any locking
+    if((queue_back + 1) % MAX_QUEUED_INTERRUPTS == queue_front) {
+        // ring buffer is full, this interrupt needs to be dropped
+        return 1;
     }
-    return interrupt;
+    
+    // add to ring buffer
+    queued_interrupts[queue_back].interrupt = interrupt;
+    queued_interrupts[queue_back].error = error;
+    queue_back = (queue_back + 1) % MAX_QUEUED_INTERRUPTS;
+    
+    return 0;
+}
+
+void interrupt_dispatch_events()
+{
+    while(true) {
+        // lock this region of code so we're not racing potential interrupts
+        cli();
+        if(queue_front == queue_back) {
+            sti();
+            return;
+        }
+        uint32_t interrupt = queued_interrupts[queue_front].interrupt;
+        uint32_t error = queued_interrupts[queue_front].error;
+        queue_front = (queue_front + 1) % MAX_QUEUED_INTERRUPTS;
+        sti();
+        
+        char buff[8];
+        itoa(interrupt, buff, 10);
+        VAL isr = js_object_get(js_isr_table, js_cstring(buff));
+        VAL errcode = js_value_make_double(error);
+        if(js_value_get_type(isr) == JS_T_FUNCTION) {
+            js_call(isr, js_isr_table, 1, &errcode);
+        }
+    }
 }
