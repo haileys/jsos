e5159c7754b44d57ebe0782744738f0762980f10
Hailey Somerville <hailey@hailey.lol>
2012-02-26 00:01:26 +1100

    mouse driver

--- kernel/js/init.js
+++ kernel/js/init.js
@@ -2,13 +2,22 @@ console.log("Hello world from JavaScript!");
 
 Kernel.loadImage(Kernel.modules["/kernel/keyboard.jmg"]);
 Kernel.loadImage(Kernel.modules["/kernel/drivers.jmg"]);
-Drivers.loadDriver("ps2");
-Keyboard.addDriver(new Drivers.PS2());
+
+Drivers.loadDriver("ps2kb");
+Keyboard.addDriver(new Drivers.PS2Keyboard());
+
+Drivers.loadDriver("ps2mouse");
+var mouse = new Drivers.PS2Mouse();
+mouse.enable();
+var x = 0;
+var y = 0;
+mouse.onUpdate = function(data) {
+    x += data.x;
+    y += data.y;
+    console.log("(" + x + ", " + y + ")");
+//    console.log("[ " + (data.leftButton ? "#" : " ") + " " + (data.middleButton ? "#" : " ") + " " + (data.rightButton ? "#" : " ") + " ]");
+}
 
 Drivers.loadDriver("ide");
-
-var ide = new Drivers.IDE(0x1f0, Drivers.IDE.MASTER);
-console.log("before ata read");
-var data = ide.readSectorPIO(1);
-console.log("after ata read");
-console.log("data length: ", data);
+var hdd = new Drivers.IDE(0x1f0, Drivers.IDE.MASTER);
+hdd.readSectorPIO(1);
