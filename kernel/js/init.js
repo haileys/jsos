b46c0da03a2e77b0687e3bd41a4e0ab88c5292a8
Hailey Somerville <hailey@hailey.lol>
2012-02-25 17:06:31 +1100

    IDE driver

--- kernel/js/init.js
+++ kernel/js/init.js
@@ -5,3 +5,10 @@ Kernel.loadImage(Kernel.modules["/kernel/drivers.jmg"]);
 Drivers.loadDriver("ps2");
 Keyboard.addDriver(new Drivers.PS2());
 
+Drivers.loadDriver("ide");
+
+var ide = new Drivers.IDE(0x1f0, Drivers.IDE.MASTER);
+console.log("before ata read");
+var data = ide.readSector(1);
+console.log("after ata read");
+console.log("data length: ", data);
