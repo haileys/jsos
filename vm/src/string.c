b5a1380169215f02da24e2728e466eefd51cab51
charliesome <charlie@charliesomerville.com>
2012-02-25 22:06:43 +1100

    replace ftoa with slightly hacky homebrew dtoa

--- vm/src/string.c
+++ vm/src/string.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include "string.h"
@@ -52,153 +53,49 @@ bool js_string_eq(js_string_t* a, js_string_t* b)
     }
 #endif
 
-static void ftoa(float Value, char* Buffer)
- {
-     union
-     {
-         float f;
-     
-         struct
-         {
-             unsigned int    mantissa_lo : 16;
-             unsigned int    mantissa_hi : 7;    
-             unsigned int     exponent : 8;
-             unsigned int     sign : 1;
-         };
-     } helper;
-     
-     unsigned long mantissa;
-     signed char exponent;
-     unsigned int int_part;
-     char frac_part[3];
-     int i, count = 0;
-     
-     helper.f = Value;
-     //mantissa is LS 23 bits
-     mantissa = helper.mantissa_lo;
-     mantissa += ((unsigned long) helper.mantissa_hi << 16);
-     //add the 24th bit to get 1.mmmm^eeee format
-     mantissa += 0x00800000;
-     //exponent is biased by 127
-     exponent = (signed char) helper.exponent - 127;
-     
-     //too big to shove into 8 chars
-     if (exponent > 18)
-     {
-         Buffer[0] = 'I';
-         Buffer[1] = 'n';
-         Buffer[2] = 'f';
-         Buffer[3] = '\0';
-         return;
-     }
-     
-     //too small to resolve (resolution of 1/8)
-     if (exponent < -3)
-     {
-         Buffer[0] = '0';
-         Buffer[1] = '\0';
-         return;
-     }
-     
-     count = 0;
-     
-     //add negative sign (if applicable)
-     if (helper.sign)
-     {
-         Buffer[0] = '-';
-         count++;
-     }
-     
-     //get the integer part
-     int_part = mantissa >> (23 - exponent);    
-     //convert to string
-     itoa(int_part, &Buffer[count], 10);
-     
-     //find the end of the integer
-     for (i = 0; i < 8; i++)
-         if (Buffer[i] == '\0')
-         {
-             count = i;
-             break;
-         }        
- 
-     //not enough room in the buffer for the frac part    
-     if (count > 5)
-         return;
-     
-     //add the decimal point    
-     Buffer[count++] = '.';
-     
-     //use switch to resolve the fractional part
-     switch (0x7 & (mantissa  >> (20 - exponent)))
-     {
-         case 0:
-             frac_part[0] = '0';
-             frac_part[1] = '0';
-             frac_part[2] = '0';
-             break;
-         case 1:
-             frac_part[0] = '1';
-             frac_part[1] = '2';
-             frac_part[2] = '5';            
-             break;
-         case 2:
-             frac_part[0] = '2';
-             frac_part[1] = '5';
-             frac_part[2] = '0';            
-             break;
-         case 3:
-             frac_part[0] = '3';
-             frac_part[1] = '7';
-             frac_part[2] = '5';            
-             break;
-         case 4:
-             frac_part[0] = '5';
-             frac_part[1] = '0';
-             frac_part[2] = '0';            
-             break;
-         case 5:
-             frac_part[0] = '6';
-             frac_part[1] = '2';
-             frac_part[2] = '5';            
-             break;
-         case 6:
-             frac_part[0] = '7';
-             frac_part[1] = '5';
-             frac_part[2] = '0';            
-             break;
-         case 7:
-             frac_part[0] = '8';
-             frac_part[1] = '7';
-             frac_part[2] = '5';                    
-             break;
-     }
-     
-     //add the fractional part to the output string
-     for (i = 0; i < 3; i++)
-         if (count < 7)
-             Buffer[count++] = frac_part[i];
-     
-     //make sure the output is terminated
-     Buffer[count] = '\0';
- }
-
+    void kprintf();
 js_string_t* js_string_from_double(double number)
 {
-    char buff[200];
-    int len, i;
-    ftoa((float)number, buff);
-    len = strlen(buff);
-    if(memchr(buff, '.', len)) {
-        for(i = 1; i <= len; i++) {
-            if(buff[len - i] == '0') {
-                buff[len - i] = 0;
-            } else if(buff[len - i] == '.') {
-                buff[len - i] = 0;
-                break;
-            }
+    if(number != number) {
+        return js_cstring("NaN");
+    }
+    if(!isfinite(number)) {
+        if(number > 0) {
+            return js_cstring("Infinity");
+        } else {
+            return js_cstring("-Infinity");
         }
     }
+    double whole = floor(number);
+    double frac = fabs(number) - fabs(whole);
+    char buff[200];
+    char* ptr = buff;
+    if(whole < 0) {
+        *ptr++ = '-';
+        whole = -number;
+    }
+    if(whole == 0) {
+        *ptr++ = '0';
+    }
+    char minibuff[200];
+    memset(minibuff, 0, 200);
+    uint32_t minibuff_i = 0;
+    while(whole > 0) {
+        minibuff[minibuff_i++] = '0' + (int)fmod(whole, 10.0);
+        whole = floor(whole / 10.0);
+    }
+    while(minibuff_i > 0) {
+        *ptr++ = minibuff[--minibuff_i];
+    }
+    uint32_t frac_i;
+    if(frac > 0) {
+        *ptr++ = '.';
+        for(frac_i = 0; frac_i < 6; frac_i++) {
+            frac *= 10.0;
+            *ptr++ = '0' + (int)floor(frac);
+        }
+    }
+    *ptr++ = 0;
     return js_cstring(buff);
 }
 
