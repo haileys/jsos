13a8d054b7d41e53a00d695e9816c3ecdde93773
Hailey Somerville <hailey@hailey.lol>
2012-02-28 19:04:16 +1100

    js_gc_alloc_no_pointer

--- vm/src/string.c
+++ vm/src/string.c
@@ -10,7 +10,7 @@ js_string_t* js_string_concat(js_string_t* a, js_string_t* b)
 {
     js_string_t* str = js_alloc(sizeof(js_string_t));
     str->length = a->length + b->length;
-    str->buff = js_alloc(str->length + 1);
+    str->buff = js_alloc_no_pointer(str->length + 1);
     memcpy(str->buff, a->buff, a->length);
     memcpy(str->buff + a->length, b->buff, b->length);
     str->buff[str->length] = 0;
@@ -83,7 +83,7 @@ js_string_t* js_string_format(char* fmt, ...)
 js_string_t* js_string_vformat(char* fmt, va_list args)
 {
     js_string_t* str = js_alloc(sizeof(js_string_t));
-    str->buff = js_alloc(1024);
+    str->buff = js_alloc_no_pointer(1024);
     str->length = vsnprintf(str->buff, 1023, fmt, args);
     str->buff[1023] = 0;
     return str;
