d4a2885efb0cdcbb44e2768e841a74e8a4745ca4
Hailey Somerville <hailey@hailey.lol>
2012-02-29 21:17:01 +1100

    String.prototype.substr

--- vm/src/lib/string.c
+++ vm/src/lib/string.c
@@ -67,6 +67,34 @@ static VAL String_prototype_valueOf(js_vm_t* vm, void* state, VAL this, uint32_t
     return js_value_wrap_string(&strobj->string);
 }
 
+static VAL String_prototype_substr(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    if(js_value_get_type(this) != JS_T_STRING_OBJECT) {
+        // @TODO throw exception
+        js_panic("String.prototype.substr() is not generic");
+    }
+    js_string_object_t* str = (js_string_object_t*)js_value_get_pointer(this);
+    if(argc == 0) {
+        return this;
+    }
+    if(argc == 1) {
+        uint32_t index = js_to_uint32(argv[0]);
+        if(index >= str->string.length) {
+            return js_value_make_cstring("");
+        }
+        return js_value_make_string(str->string.buff + index, str->string.length - index);
+    }
+    uint32_t index = js_to_uint32(argv[0]);
+    uint32_t length = js_to_uint32(argv[1]);
+    if(index >= str->string.length) {
+        return js_value_make_cstring("");
+    }
+    if(index + length >= str->string.length) {
+        length = str->string.length - index;
+    }
+    return js_value_make_string(str->string.buff + index, length);
+}
+
 void js_lib_string_initialize(js_vm_t* vm)
 {
     vm->lib.String = js_value_make_native_function(vm, NULL, js_cstring("String"), String_call, String_construct);
@@ -77,4 +105,5 @@ void js_lib_string_initialize(js_vm_t* vm)
     
     js_object_put(vm->lib.String_prototype, js_cstring("toString"), js_value_make_native_function(vm, NULL, js_cstring("toString"), String_prototype_toString, NULL));
     js_object_put(vm->lib.String_prototype, js_cstring("valueOf"), js_value_make_native_function(vm, NULL, js_cstring("valueOf"), String_prototype_valueOf, NULL));
+    js_object_put(vm->lib.String_prototype, js_cstring("substr"), js_value_make_native_function(vm, NULL, js_cstring("substr"), String_prototype_substr, NULL));
 }
