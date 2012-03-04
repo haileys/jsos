f29355e6155ae3f23f7ed570173579a5b00bc316
Hailey Somerville <hailey@hailey.lol>
2012-03-04 20:17:04 +1100

    paging_set_directory asm routine

--- kernel/src/helpers.asm
+++ kernel/src/helpers.asm
@@ -1,4 +1,5 @@
 global gdt_reload_segment_registers
+global paging_set_directory
 
 gdt_reload_segment_registers:
     jmp 0x08:.flush_cs
@@ -9,4 +10,15 @@ gdt_reload_segment_registers:
     mov fs, ax
     mov gs, ax
     mov ss, ax
+    ret
+
+paging_set_directory:
+    ; page directory address:
+    mov eax, [esp+4]
+    mov cr3, eax
+    ; enable paging
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+    
     ret
