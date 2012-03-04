c65b829011dce92ef32d7c2a05aa214d59dbd71f
charliesome <charlie@charliesomerville.com>
2012-03-04 20:17:58 +1100

    limit code segment to end of multiboot loaded image

--- kernel/src/gdt.c
+++ kernel/src/gdt.c
@@ -3,14 +3,18 @@
 static gdt_entry_t* gdt = (gdt_entry_t*)0xf000;
 static gdtr_t gdtr;
 
+static int end_of_image;
+
 void gdt_init()
-{    
+{
+    uint32_t code_end = ((uint32_t)&end_of_image + 4095) / 4096;
+    
     // 0x08 is a code selector:
-    gdt[1].limit_0_15               = 0xffff;
+    gdt[1].limit_0_15               = code_end & 0xffff;
     gdt[1].base_0_15                = 0x0000;
     gdt[1].base_16_23               = 0x00;
     gdt[1].access                   = 0x02 /* readable */ | 0x08 /* code segment */ | 0x10 /* reserved */ | 0x80 /* present */;
-    gdt[1].limit_16_19_and_flags    = 0xf | 0x40 /* 32 bit */ | 0x80 /* 4 KiB */;
+    gdt[1].limit_16_19_and_flags    = ((code_end >> 16) & 0xf) | 0x40 /* 32 bit */ | 0x80 /* 4 KiB */;
     gdt[1].base_24_31               = 0x00;
     
     // 0x10 is a data selector:
