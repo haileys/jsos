e9540b0c00df5f7999836b726b785d64c147a93f
charliesome <charlie@charliesomerville.com>
2012-03-04 13:43:50 +1100

    pluggable driver loader

--- kernel/js/kernel/drivers.js
+++ kernel/js/kernel/drivers.js
@@ -1,11 +1,18 @@
 (function() {
+    var driverLoader = function(name) {
+        return Kernel.modules[name];
+    };
+    
     Drivers = {
         loadDriver: function(driverName) {
-            var mod = Kernel.modules["/kernel/drivers/" + driverName + ".jmg"];
+            var mod = driverLoader("/kernel/drivers/" + driverName + ".jmg");
             if(typeof mod !== "string") {
                 throw new Error("Could not load driver '" + driverName + "'");
             }
             Kernel.loadImage(mod);
+        },
+        setLoader: function(loader) {
+            driverLoader = loader;
         }
     };
 })();
