e8627edfa2034ba3b4b2a4536a86b7b00ed000ef
charliesome <charlie@charliesomerville.com>
2012-03-09 20:08:48 +1100

    extra methods on VM object, better type checking

--- kernel/src/lib/vm.c
+++ kernel/src/lib/vm.c
@@ -10,7 +10,9 @@ static VAL VM_prototype;
 
 static js_vm_t* get_vm(js_vm_t* vm, VAL this)
 {
-    if(js_value_get_type(this) != JS_T_OBJECT || js_value_get_pointer(js_value_get_pointer(this)->object.class) != js_value_get_pointer(VM)) {
+    if(js_value_get_type(this) != JS_T_OBJECT
+    || js_value_get_pointer(js_value_get_pointer(this)->object.class) != js_value_get_pointer(VM)
+    || js_value_get_pointer(js_value_get_pointer(this)->object.prototype) != js_value_get_pointer(VM_prototype)) {
         js_throw_error(vm->lib.TypeError, "Can't call VM instance methods with non-VM as object");
     }
     return (js_vm_t*)js_value_get_pointer(this)->object.state;
@@ -25,13 +27,41 @@ static VAL wrap_vm(js_vm_t* vm)
 
 static VAL VM_construct(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
 {
-    if(argc == 0 || (js_value_get_type(argv[0]) != JS_T_STRING && js_value_get_type(argv[0]) != JS_T_FUNCTION)) {
-        js_throw_error(vm->lib.TypeError, "VM constructor expects either a function or image data");
-    }
     js_vm_t* new_vm = js_vm_new();
-    // @TODO make this go through a system call:
-    console_init(new_vm);
-    lib_vm_init(new_vm);
+    return wrap_vm(new_vm);
+}
+
+static VAL VM_self(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    return wrap_vm(vm);
+}
+
+static VAL VM_prototype_id(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    return js_value_make_double((uint32_t)get_vm(vm, this));
+}
+
+static VAL VM_prototype_globals(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    return get_vm(vm, this)->global_scope->global_object;
+}
+
+static VAL VM_prototype_globals_set(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    if(argc == 0) {
+        js_throw_error(vm->lib.TypeError, "expected value as first argument");
+    }
+    return get_vm(vm, this)->global_scope->global_object = argv[0];
+}
+
+static VAL VM_prototype_execute(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    if(argc == 0 || (js_value_get_type(argv[0]) != JS_T_STRING && js_value_get_type(argv[0]) != JS_T_FUNCTION)) {
+        js_throw_error(vm->lib.TypeError, "VM.prototype.execute expects either a function or image data");
+    }
+    
+    js_vm_t* target_vm = get_vm(vm, this);
+    
     js_image_t* image;
     uint32_t section;
     if(js_value_get_type(argv[0]) == JS_T_STRING) {
@@ -46,23 +76,13 @@ static VAL VM_construct(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* 
         // run section in existing image
         js_function_t* fn = (js_function_t*)js_value_get_pointer(argv[0]);
         if(fn->is_native) {
-            js_throw_error(vm->lib.TypeError, "Can't create VM from native function");
+            js_throw_error(target_vm->lib.TypeError, "Can't create VM from native function");
         }
         image = fn->js.image;
         section = fn->js.section;
     }
-    js_vm_exec(new_vm, image, section, new_vm->global_scope, js_value_null(), 0, NULL);
-    return wrap_vm(new_vm);
-}
-
-static VAL VM_self(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
-{
-    return wrap_vm(vm);
-}
-
-static VAL VM_prototype_id(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
-{
-    return js_value_make_double((uint32_t)get_vm(vm, this));
+    
+    return js_vm_exec(target_vm, image, section, target_vm->global_scope, js_value_null(), 0, NULL);
 }
 
 void lib_vm_init(js_vm_t* vm)
@@ -73,9 +93,11 @@ void lib_vm_init(js_vm_t* vm)
     js_gc_register_global(&VM_prototype, sizeof(VM_prototype));
     js_object_put(vm->global_scope->global_object, js_cstring("VM"), VM);
     
-    // class methods:
-    js_object_put(VM, js_cstring("self"), js_value_make_native_function(vm, NULL, js_cstring("self"), VM_self, NULL));
+    // class properties:
+    js_object_put_accessor(vm, VM, "self", VM_self, NULL);
+    js_object_put_accessor(vm, VM_prototype, "id", VM_prototype_id, NULL);
+    js_object_put_accessor(vm, VM_prototype, "globals", VM_prototype_globals, VM_prototype_globals_set);
     
     // instance methods:
-    js_object_put(VM_prototype, js_cstring("id"), js_value_make_native_function(vm, NULL, js_cstring("id"), VM_prototype_id, NULL));
+    js_object_put(VM_prototype, js_cstring("execute"), js_value_make_native_function(vm, NULL, js_cstring("execute"), VM_prototype_execute, NULL));
 }
