6f3aae1b23d1e8e717b6f60435a830c5784d5748
Hailey Somerville <hailey@hailey.lol>
2012-03-09 22:18:58 +1100

    pit driver

---
+++ kernel/js/kernel/drivers/pit.js
@@ -0,0 +1,26 @@
+(function() {
+    var PIT_CLOCK_FREQ = 1193180;
+    
+    var listeners = [];
+    Drivers.PIT = {
+        init: function(frequency) {
+            var divisor = PIT_CLOCK_FREQ / frequency;
+            if(divisor & ~0xffff) {
+                throw new RangeError("PIT frequency too low");
+            }
+            
+            Kernel.outb(0x43, 0x36);
+            Kernel.outb(0x40, divisor & 0xff);
+            Kernel.outb(0x40, (divisor >> 8) & 0xff);
+            
+            Kernel.isrs[32] = function() {
+                for(var i = 0; i < listeners.length; i++) {
+                    listeners[i]();
+                }
+            };
+        },
+        subscribe: function(callback) {
+            listeners.push(callback);
+        }
+    };
+})();
