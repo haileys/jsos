eb14e4f7b592a2d44ce94509649fc7814fa58574
charliesome <charlie@charliesomerville.com>
2012-03-09 23:53:45 +1100

    libc sin, cos, sqrt

--- libc/inc/math.h
+++ libc/inc/math.h
@@ -3,6 +3,7 @@
 
 #define NAN (0.0/0.0)
 #define INFINITY (1.0/0.0)
+#define M_PI (3.14159265358979323846)
 
 int __inline_isfinite_double(double x);
 
@@ -11,5 +12,8 @@ int __inline_isfinite_double(double x);
 double floor(double x);
 double fmod(double x, double y);
 double fabs(double x);
+double cos(double x);
+double sin(double x);
+double sqrt(double x);
 
 #endif
