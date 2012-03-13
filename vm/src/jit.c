d8f26fd1665deb99cad7456dba1dc05fcd793ce0
charliesome <charlie@charliesomerville.com>
2012-03-13 19:45:49 +1100

    pushnum instruction in jit

--- vm/src/jit.c
+++ vm/src/jit.c
@@ -16,11 +16,14 @@
         idx += len; \
     } while(0)
 
-VAL wat(struct js_vm* a, void* b, VAL c, uint32_t d, VAL* e) { return js_value_undefined(); }
+#define APPEND_U32(u32) do { \
+        RESIZE_BUFF(idx + 4); \
+        *(uint32_t*)(buff + idx) = (u32); \
+        idx += 4; \
+    } while(0)
 
 js_native_callback_t js_jit_section(js_section_t* section, uint32_t* length)
 {
-    uint32_t ins_i;
     uint32_t idx = 0;
     uint32_t capacity = 32;
     uint8_t* buff = js_alloc(32);
@@ -29,8 +32,9 @@ js_native_callback_t js_jit_section(js_section_t* section, uint32_t* length)
     // 89EC              mov ebp,esp
     APPEND("\x55\x89\xE5", 3);
     
-    for(ins_i = 0; ins_i < section->instruction_count; ins_i++) {
-        switch(section->instructions[ins_i]) {
+    uint32_t ins_i = 0;
+    while(ins_i < section->instruction_count) {
+        switch(section->instructions[ins_i++]) {
             
             case JS_OP_UNDEFINED: {
                 // 680000FAFF        push dword 0xfffa0000
@@ -48,6 +52,18 @@ js_native_callback_t js_jit_section(js_section_t* section, uint32_t* length)
                 // C9                leave
                 // C20400            ret
                 APPEND("\x8B\x45\x08\x59\x89\x08\x59\x89\x48\x04\xC9\xC2\x04\x00", 14);
+                break;
+            }
+            
+            case JS_OP_PUSHNUM: {
+                // 68xxxxxxxx        push dword HIGH_DWORD
+                // 68xxxxxxxx        push dword LOW_DWORD
+                uint32_t lo = section->instructions[ins_i++];
+                uint32_t hi = section->instructions[ins_i++];
+                APPEND("\x68", 1);
+                APPEND_U32(hi);
+                APPEND("\x68", 1);
+                APPEND_U32(lo);
                 break;
             }
             
