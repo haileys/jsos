d5b24fcd6062bb183d6090507f234f61d88a4a92
Hailey Somerville <hailey@hailey.lol>
2012-03-25 18:25:27 +1100

    change ps2kb's enable method to init, reinitialize the keyboard device

--- kernel/js/kernel/drivers/ps2kb.js
+++ kernel/js/kernel/drivers/ps2kb.js
@@ -1,15 +1,15 @@
 (function() {
     function PS2Keyboard() { }
     
-    PS2Keyboard.prototype.enable = function() {
+    PS2Keyboard.prototype.init = function() {
         var self = this;
         Kernel.isrs[33] = function() {
             self.onInterrupt();
         };
-    };
-    
-    PS2Keyboard.prototype.disable = function() {
-        Kernel.isrs[33] = null;
+        // disable and re-enable keyboard device
+        var b = Kernel.inb(0x61);
+        Kernel.outb(0x61, b | 0x80);
+        Kernel.outb(0x61, b & 0x7f);
     };
     
     PS2Keyboard.prototype.onInterrupt = function() {
