91057b39a5b8f78e70a73b047cce6d9a9ff0b4dc
charliesome <charlie@charliesomerville.com>
2012-05-03 21:20:45 +1000

    rewrite init.bin in coffeescript

--- kernel/js/bin/init.js
+++
@@ -1,6 +0,0 @@
-OS.write(OS.stdout, "\nListing contents of /:\n");
-var entries = OS.readDirectory("/");
-for(var i = 0; i < entries.length; i++) {
-    OS.write(OS.stdout, "  - " + entries[i].name + "\n");
-}
-
