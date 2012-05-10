a586638adccfd17b0e22990c692f62d4593ab7e4
Hailey Somerville <hailey@hailey.lol>
2012-05-10 22:11:38 +1000

    panic if interrupt buffer fills up

--- kernel/js/kernel/drivers/ps2kb.js
+++ kernel/js/kernel/drivers/ps2kb.js
@@ -6,16 +6,11 @@
         Kernel.isrs[33] = function() {
             self.onInterrupt();
         };
-        // disable and re-enable keyboard device
-//        var b = Kernel.inb(0x61);
-//        Kernel.outb(0x61, b | 0x80);
-//        Kernel.outb(0x61, b & 0x7f);
-        for(var i = 0; i < 16; i++) {
-            Kernel.inb(0x60);
-        }
+        Kernel.outb(0x20, 0x20);
     };
     
     PS2Keyboard.prototype.onInterrupt = function() {
+        while(Kernel.inb(0x64) & 2) ;
         var scanCode = Kernel.inb(0x60);
         if(typeof this.onScanCode === "function") {
             this.onScanCode(scanCode);
