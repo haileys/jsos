3f24704600f8dcc232493b3faf4ac6b43287ef9c
charliesome <charlie@charliesomerville.com>
2012-05-10 20:25:00 +1000

    expose Math.pow

--- vm/src/lib/math.c
+++ vm/src/lib/math.c
@@ -33,6 +33,13 @@ static VAL Math_sqrt(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* arg
     return js_value_make_double(sqrt(d));
 }
 
+static VAL Math_pow(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
+{
+    double a = js_value_get_double(js_to_number(argc >= 1 ? argv[0] : js_value_undefined()));
+    double b = js_value_get_double(js_to_number(argc >= 2 ? argv[1] : js_value_undefined()));
+    return js_value_make_double(pow(a, b));
+}
+
 static VAL Math_abs(js_vm_t* vm, void* state, VAL this, uint32_t argc, VAL* argv)
 {
     double d = js_value_get_double(js_to_number(argc ? argv[0] : js_value_undefined()));
@@ -93,6 +100,7 @@ void js_lib_math_initialize(js_vm_t* vm)
     js_object_put(Math, js_cstring("cos"), js_value_make_native_function(vm, NULL, js_cstring("cos"), Math_cos, NULL));
     js_object_put(Math, js_cstring("sin"), js_value_make_native_function(vm, NULL, js_cstring("sin"), Math_sin, NULL));
     js_object_put(Math, js_cstring("sqrt"), js_value_make_native_function(vm, NULL, js_cstring("sqrt"), Math_sqrt, NULL));
+    js_object_put(Math, js_cstring("pow"), js_value_make_native_function(vm, NULL, js_cstring("pow"), Math_pow, NULL));
     js_object_put(Math, js_cstring("abs"), js_value_make_native_function(vm, NULL, js_cstring("abs"), Math_abs, NULL));
     js_object_put(Math, js_cstring("random"), js_value_make_native_function(vm, NULL, js_cstring("random"), Math_random, NULL));
     js_object_put(Math, js_cstring("PI"), js_value_make_double(M_PI));
