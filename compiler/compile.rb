11c4b4ef7a28bff06faa58a2f79d0aafb884756f
Hailey Somerville <hailey@hailey.lol>
2012-05-13 15:08:49 +1000

    jsx is the name of the executable format, so I've changed it from binary_compiler.rb to jsx_compiler.rb

--- compiler/compile.rb
+++ compiler/compile.rb
@@ -1,12 +1,12 @@
 $LOAD_PATH << File.expand_path("../../twostroke/lib", __FILE__)
 require "twostroke"
-require File.expand_path("../binary_compiler", __FILE__)
+require File.expand_path("../jsx_compiler", __FILE__)
 
 file = File.open ARGV.first, "r"
 parser = Twostroke::Parser.new Twostroke::Lexer.new file.read
 parser.parse
 
-compiler = JSOS::BinaryCompiler.new parser.statements, ARGV.last
+compiler = JSOS::JSXCompiler.new parser.statements, ARGV.last
 compiler.compile
 
 print compiler.bytecode
