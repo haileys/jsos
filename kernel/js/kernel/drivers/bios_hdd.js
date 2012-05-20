bd69838530d035a0e3455ac02fb6fbdf684e4dea
Hailey Somerville <hailey@hailey.lol>
2012-05-20 20:09:05 +1000

    move bios hdd driver's data packet out of the zero page

--- kernel/js/kernel/drivers/bios_hdd.js
+++ kernel/js/kernel/drivers/bios_hdd.js
@@ -16,15 +16,15 @@
             throw new TypeError("expected lba to be a number");
         }
         // construct LBA packet
-        Kernel.poke8(0x500 + 0, 16 /* packet size */);
-        Kernel.poke8(0x500 + 1, 0 /* reserved */);
-        Kernel.poke16(0x500 + 2, 1 /* number of sectors */);
-        Kernel.poke32(0x500 + 4, 0x1000 /* read buffer address */);
-        Kernel.poke32(0x500 + 8, lba);
-        Kernel.poke32(0x500 + 12, 0 /* upper part of 48 bit lba - ignore */);
+        Kernel.poke8(0xa000 + 0, 16 /* packet size */);
+        Kernel.poke8(0xa000 + 1, 0 /* reserved */);
+        Kernel.poke16(0xa000 + 2, 1 /* number of sectors */);
+        Kernel.poke32(0xa000 + 4, 0x1000 /* read buffer address */);
+        Kernel.poke32(0xa000 + 8, lba);
+        Kernel.poke32(0xa000 + 12, 0 /* upper part of 48 bit lba - ignore */);
         
         var code =  /* mov ah, 0x42 */   "\xB4\x42" +
-                    /* mov si, 0x500 */  "\xBE\x00\x05" +
+                    /* mov si, 0xa000 */ "\xBE\x00\xa0" +
                     /* mov bx, 0x55aa */ "\xBB\xAA\x55" +
                     /* mov dl, 0x80 */   "\xB2" + String.fromCharCode(this.driveNumber) +
                     /* int 0x13 */       "\xCD\x13" +
