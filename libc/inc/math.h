c0c5a5088d6bb3b6225daa7773855792bba8c658
Hailey Somerville <hailey@hailey.lol>
2012-06-01 23:37:16 +1000

    inline isfinite test

--- libc/inc/math.h
+++ libc/inc/math.h
@@ -5,9 +5,7 @@
 #define INFINITY (1.0/0.0)
 #define M_PI (3.14159265358979323846)
 
-int __inline_isfinite_double(double x);
-
-#define isfinite(x) __inline_isfinite_double(x)
+#define isfinite(x) ((x) == (x) && (x) != INFINITY && (x) != -INFINITY)
 
 double floor(double x);
 double fmod(double x, double y);
